f8bbcbda3e156f24baafe4f2071e0b49d61563f3
peondusud <reflexion_31@hotmail.com>
2013-08-10 20:10:52 +0200

    Update write_meta.py

--- write_meta.py
+++ write_meta.py
@@ -1,1 +1,8 @@
 import meta
+
+
+
+source = "source.mkv" 
+new_file ="RENAME.mkv"
+IMDB_id = "145852"
+addIMDB = (ffmpeg -i str(source) -vcodec copy -acodec copy -metadata IMDB=str(IMDB_id) str(new_file))
