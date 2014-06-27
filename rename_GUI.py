74f3db5df6225c778e38e606be4023f8813b66c9
peondusud <reflexion_31@hotmail.com>
2014-06-27 18:02:22 +0200

    Update rename_GUI.py

--- rename_GUI.py
+++ rename_GUI.py
@@ -2,32 +2,28 @@
 """
 TODO
 exception for windows long path+filename
-check if filename  is already in root/top folder so dont parent folder move
+check if filename  is already in root/top folder so dont move to parent folder
 """
 import os
 import re
 import sys
+import string
 import time
 import shutil
 import stat
+import Tkinter as tk
 
-file_type=['mp4','mkv','m2t','m2ts','ts','avi']
-noTix_flag=0
-try:
-	import Tix
-	root = Tix.Tcl()
-	del(root)
-	
-except Exception as e:
-	print e
-	import Tkinter as Tix
-	noTix_flag=1
-	print "Warning : Next time install \"tix\" package"
+import tkFileDialog
+
+video_ext = ['mp4','mkv','m2t','m2ts','ts','avi']
+subttile_ext= ['srt','vtt']
+file_type= video_ext + map(string.upper, video_ext)
+
 
 def get_parent_folder(file_path):
-	dir = os.path.dirname(file_path)
+	dirz = os.path.dirname(file_path)
 	#dir = os.path.abspath(os.path.join(f_path, os.pardir))
-	return os.path.basename(os.path.normpath(dir))
+	return os.path.basename(os.path.normpath(dirz))
 
 def get_file_extension(file_path):
 	fileExtension = os.path.splitext(file_path)[1]
@@ -103,11 +99,10 @@ def check_bigger_size_file_folder_name(file_path):
 		rename_filename_by_folder(file_path)
 
 
-def list_folder(top,dont_ask=True):
+def list_folder(top):
 	"""
 		arg:
 			top = top path directory
-			dont_ask = don't ask the user to rename each file
 		function:
 			list files in a folder
 			and for each file
@@ -129,34 +124,46 @@ def list_folder(top,dont_ask=True):
 			rm_files_into_dir(top)
 			os.rmdir(os.path.join(root, dir))
 
+def select_dir():
+	global win
+	win2 = tk.Tk()
+	win2.withdraw()
+	dirname = tkFileDialog.askdirectory(parent=win2,initialdir="/",title='Please select a directory')
+	if os.path.exists(dirname):
+		if os.path.isdir(dirname):			
+			list_folder(dirname)
+	entry.delete(0, tk.END)
+	entry.insert(0, dirname)	
+	win.deiconify() # show again
 
 def onClick():
 	top=entry.get()
 	if top is None:
-		print "Erreur : path not found"
+		print "Error : path not found"
 		sys.exit(0)
 	else:
-		list_folder(top,False)
+		if os.path.exists(top):
+			if os.path.isdir(top):
+				list_folder(top)
 		
-	win.destroy()
-	#mainwin.destroy() # uncomment to close after complete download
+	#win.destroy() # uncomment to close after complete
 
 
 if __name__ == "__main__":
-	win = Tix.Tk()
+	win = tk.Tk()
 	win.title("rename files by subfolders !")
-	frame = Tix.Frame(win, width=768, height=576, borderwidth=1)
+	frame = tk.Frame(win, width=768, height=576, borderwidth=1)
 	frame.pack()
-	lbl = Tix.Label(frame,text="root folder !")
+	lbl = tk.Label(frame,text="Select root folder !")
 	lbl.pack() #attach label to window
 
-
-	entry = Tix.Entry(frame , width=90)
+	#dirz= StringVar()
+	entry = tk.Entry(frame , width=90)
 	entry.pack()
 	entry.focus_set()
 	path = "D:\Revenge.S01.FRENCH.DVDRiP.XViD-EPZ"
 	entry.insert(0, path)
-
-	bouton= Tix.Button(frame, text="Valid", command=onClick)
-	bouton.pack()
+	tk.Button(frame,text='Select Folder',command=select_dir).pack(side=tk.RIGHT)
+	
+	tk.Button(frame, text="Valid", command=onClick).pack(fill=tk.BOTH,expand=tk.YES)
 	win.mainloop()
